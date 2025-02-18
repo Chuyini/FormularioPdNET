8db53ab2a30813f95dff9db390de2ec88325ce83
Chuyini <jmlr231201@gmail.com>
2025-02-18 14:10:43 -0600

    implemente router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePage } from './pages/home/home.page';
+import { GratitudePage } from './pages/gratitude/gratitude.page';
 
 @Component({
   selector: 'app-root',
-  imports: [HomePage],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomePage } from './pages/home/home.page';
+import { GratitudePage } from './pages/gratitude/gratitude.page';
+export const routes: Routes = [
+    {
+        path: "", component: HomePage
+    },
+    { path: "gratitude", component: GratitudePage }
+];
