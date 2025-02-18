8c58d47662dcb4d9a9a0dd7bcbbc75005aef1aeb
Chuyini <jmlr231201@gmail.com>
2025-02-18 14:09:10 -0600

    implemente router outlet

---
+++ src/app/pages/gratitude/gratitude.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gratitude',
+  imports: [],
+  templateUrl: './gratitude.page.html',
+  styleUrl: './gratitude.page.css'
+})
+export class GratitudePage {
+
+}
