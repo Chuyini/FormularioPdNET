7bd70f8af72bddcb067a2fb6ca8d99c72c4c43a9
Chuyini <jmlr231201@gmail.com>
2025-03-29 13:26:59 -0600

    Changgue de endpoint

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -608,9 +608,9 @@ export class FormComponent {
       };
 
       // Enviar la petición al servidor 
-      
+
       //https://email-own.vercel.app/send-email
-      axios.post('http://emailown-production.up.railway.app/', body)
+      axios.post('https://emailown-production.up.railway.app/', body)
         .then(response => {
           console.log('Archivos enviados exitosamente:', response);
           this.router.navigate(['/gratitude']);
