cbaa699beaa42d687a0f129544fa660b38611beb
Chuyini <jmlr231201@gmail.com>
2025-04-24 14:31:52 -0600

    razon social

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -537,7 +537,7 @@ export class FormComponent {
       const body = {
         to: emails,
         subject: 'ALTA DE CLIENTES',
-        text: '¡Hola! te entrego el Alta de clientes 😊👌 ➡️',
+        text: `¡Hola! te entrego el Alta de clientes: ${this.rasonName} 😊👌 ➡️`,
         attachments: attachmentsArray,
         variables: [
           {
