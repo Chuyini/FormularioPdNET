d43d9027845b2f496b86b36f738222a98413ed74
chuyini <jmlr231201@gmail.com>
2025-11-01 10:00:39 -0600

    try to transalate

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -213,11 +213,7 @@ export class FormComponent {
     { "id": 21, "codigo": "31", "descripcion": "Intermediario pagos" },
     { "id": 22, "codigo": "99", "descripcion": "Por definir" }
   ]
-  public translatedWayPage = this.wayPage.map(item => ({
-    ...item,
-    label: this.translocoService.translate(item.descripcion)
-  }));
-
+  
 
 
   banks = [
